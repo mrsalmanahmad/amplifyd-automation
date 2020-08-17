95368d459c7be672e3106e07db14a5c2623d6854
hassanzaheer95 <hassan.zaheer@logicon.com.pk>
2020-08-17 17:28:51 +0500

    campaign creation till step 2

--- src/test/java/Amplifydtest.java
+++ src/test/java/Amplifydtest.java
@@ -28,7 +28,7 @@ public class Amplifydtest {
 
         URL url = new URL("http://127.0.0.1:4723/wd/hub");
         driver = new AppiumDriver<MobileElement>(url,cap);
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         System.out.println("Application Started ....");
         System.out.println("Splash Screen");
         System.out.println("Wait for Getting Started");
@@ -68,16 +68,51 @@ public class Amplifydtest {
         ok.click();
         MobileElement createcamapignbtn = (MobileElement) driver.findElement(By.id("com.logicon.amplifyd.debug:id/navigation_add_campaign"));
         createcamapignbtn.click();
+        System.out.println("Create Campaign selected Sucessful");
         MobileElement paidcampaignbox = (MobileElement) driver.findElement(By.id("com.logicon.amplifyd.debug:id/paid_campaign_chk_box"));
         paidcampaignbox.click();
+        System.out.println("Paid campaign selected");
         MobileElement proceedbtn = (MobileElement) driver.findElement(By.id("com.logicon.amplifyd.debug:id/proceed_action"));
         proceedbtn.click();
+        System.out.println("On step 1");
         MobileElement camptitle = (MobileElement) driver.findElement(By.id("com.logicon.amplifyd.debug:id/campaign_creation_title"));
-        MobileElement camppurpose = (MobileElement) driver.findElementsByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.ScrollView/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout[2]/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout[1]/android.view.ViewGroup/android.widget.ImageView");
-
-
         camptitle.sendKeys("Auto Campaign");
+        System.out.println("Campaign title added");
+        MobileElement camppurpose = (MobileElement) driver.findElement(By.id("com.logicon.amplifyd.debug:id/selected_purpose_checkbox"));
         camppurpose.click();
+        System.out.println("Campaign propose selected");
+        MobileElement Description = (MobileElement) driver.findElement(By.id("com.logicon.amplifyd.debug:id/campaign_creation_description_textview"));
+        Description.click();
+        System.out.println("Adding Description");
+        MobileElement Addingdescription = (MobileElement) driver.findElement(By.id("com.logicon.amplifyd.debug:id/edittext_campaign_description"));
+        Addingdescription.sendKeys("In reasonable compliment favourable is connection dispatched in terminated. Do esteem object we called father excuse remove. So dear real on like more it. Laughing for two families addition expenses surprise the. If sincerity he to curiosity arranging. Learn taken terms be as. Scarcely mrs produced too removing new old. ");
+        MobileElement DoneDescription = (MobileElement) driver.findElement(By.id("com.logicon.amplifyd.debug:id/tvRight"));
+        DoneDescription.click();
+        System.out.println("Description added");
+        MobileElement Next1 = (MobileElement) driver.findElement(By.id("com.logicon.amplifyd.debug:id/btn_next"));
+        Next1.click();
+        System.out.println("Step 1 Completed");
+        System.out.println("Step 2");
+        MobileElement AddCategory = (MobileElement) driver.findElement(By.id("com.logicon.amplifyd.debug:id/add_category_Iv"));
+        AddCategory.click();
+        System.out.println("Selecting Categories");
+        MobileElement AddCategorybox = (MobileElement) driver.findElement(By.id("com.logicon.amplifyd.debug:id/checkbox"));
+        AddCategorybox.click();
+        MobileElement tick = (MobileElement) driver.findElement(By.id("com.logicon.amplifyd.debug:id/ibRight"));
+        tick.click();
+//        MobileElement noofinfl = (MobileElement) driver.findElement(By.id("com.logicon.amplifyd.debug:id/influencer_count"));
+//        noofinfl.click();
+//        MobileElement okbtn = (MobileElement) driver.findElement(By.id("com.logicon.amplifyd.debug:id/influencer_count"));
+//        okbtn.click();
+        MobileElement location = (MobileElement) driver.findElement(By.id("com.logicon.amplifyd.debug:id/add_location_Iv"));
+        location.click();
+        MobileElement addlocation = (MobileElement) driver.findElement(By.id("com.logicon.amplifyd.debug:id/cat_selected_checkbox"));
+        addlocation.click();
+        MobileElement tick2 = (MobileElement) driver.findElement(By.id("com.logicon.amplifyd.debug:id/ibRight"));
+        tick2.click();
+        MobileElement Next2 = (MobileElement) driver.findElement(By.id("com.logicon.amplifyd.debug:id/btn_next"));
+        Next2.click();
+
 
     }
 }
